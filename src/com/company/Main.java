c4caa5fb74682934fed5317b65b7288c02a57d7f
ls059 <lyspers@gmail.com>
2017-12-19 08:51:04 -0500

    move 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -26,11 +26,22 @@ public class Main {
             boardState.uMove[i] = Integer.parseInt(str[i]);
         }
         boardState.userMove(boardState.uMove[0], boardState.uMove[1]);
-
         System.out.println("Thinking...");
-
         Thread.sleep(2000);
+        boardState.cpuMove();
 
+
+
+        move = new Scanner(System.in);
+        System.out.println("Please input the coordinates of your move with 0,0 being the top left and 2,2 being the bottom right.\nex.  1,1 or 0,2");
+        in = move.next();
+        str = in.split(",");
+        for(int i = 0; i < str.length; i++) {
+            boardState.uMove[i] = Integer.parseInt(str[i]);
+        }
+        boardState.userMove(boardState.uMove[0], boardState.uMove[1]);
+        System.out.println("Thinking...");
+        Thread.sleep(2000);
         boardState.cpuMove();
 
 
--- src/com/company/boardState.java
+++ src/com/company/boardState.java
@@ -9,6 +9,7 @@ public class boardState  {
     public static int[] uMove = new int[2];
     public static int[] cMove = new int[2];
     static String cpuL;
+    static String userL;
     public boardState(){
 
         board = new String[3][3];
@@ -18,11 +19,13 @@ public class boardState  {
             }
         }
     }
-    public static void userMove(int x, int y){
+    public static void userMove(int y, int x){
         if(move%2 == 0) {
             board[y][x] = "o ";
+            userL = "o ";
         }else{
             board[y][x] = "x ";
+            userL = "x ";
         }
         for(int i = 0; i<boardState.board.length; i++) {
             for(int j = 0; j<boardState.board.length; j++) {
@@ -60,12 +63,13 @@ public class boardState  {
 
         }
 
-        /*
-        cMove = check2(uMove[0], uMove[1]);
-        System.out.println("I will take(" + cMove[0]+ "," + cMove[1]+ ")");
-        board[cMove[0]][cMove[1]] = cpuL;
-        //check for 2 in a row etc.
-        */
+
+        if(move > 2) {
+            cMove = check2(uMove[0], uMove[1]);
+            System.out.println("I will take(" + cMove[0] + "," + cMove[1] + ")");
+            board[cMove[0]][cMove[1]] = cpuL;
+            //check for 2 in a row etc.
+        }
 
         for(int i = 0; i<boardState.board.length; i++) {
             for(int j = 0; j<boardState.board.length; j++) {
@@ -80,33 +84,40 @@ public class boardState  {
         }
 
         move++;
+
+
     }
 
-   /* private static int[] check2(int x, int y){
-
+   private static int[] check2(int x, int y){
+       int[] a = new int[2];
         int countU = 0, countC = 0;
-        if(y == 0 && x == 0 || y == 0 && x == 2 || y == 2 && x == 0 || y == 2 && x == 2){
-            System.out.println("Safe Up");
+
+            for(int k = 0;k<board.length;k++){
             for(int i=0;i<board.length;i++){
-                if (!(board[0][i].equals(cpuL))){
+                if (!(board[k][i].equals(cpuL))) {
                     countU++;
+                }else if((board[0][i].equals(cpuL))){
+                        countC++;
+                    }
+                    if(countU == 2 && !(countC == 1)){
+                        for(int j = 0;j<board.length;j++){
+                            if(!(board[k][j].equals(userL)));{
+                                a[0] = k; a[1] = j;
+                                System.out.println(a[0] + " " + a[1]);
+                                return a;
+                            }
+
+                        }
+                    }
                 }
-                else if((board[0][i].equals(cpuL))){
-                    countC++;
-                }
+                countU = 0;
             }
-            if(countU == 2 && !(countC == 1)){
 
 
 
-            }
+       return a;
         }
-        else if(board[y-1][x].equals("- ")){
-            System.out.println("safe up");
-
-        }
-      return [];
 
     }
-*/
-}
+
+
